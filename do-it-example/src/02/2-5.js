32dd7736409d83b0b89e8b525540fff03f078b9b
suinautant <suinautant@gmail.com>
2021-11-05 07:52:50 +0900

    2-5

---
+++ do-it-example/src/02/2-5.js
@@ -0,0 +1,8 @@
+function add(first, second) {
+  return first + second;
+}
+//typeof add == 'function'
+var add = function (first, second) {
+  return first + second;
+};
+// typeof add == 'function'
