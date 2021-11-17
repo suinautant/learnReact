48aae5d863b374dabac0aaf61cb8946f9c940985
suinautant <suinautant@gmail.com>
2021-11-17 13:30:54 +0900

    컴포넌트 생명주기 함수 살펴보기

---
+++ do-it-example/src/03/LifecycleExample.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class LifecycleExample extends React.Component {
+  static getDerivedStateFromProps() {
+    console.log('getDerivedStateFromProps 호출');
+    return {};
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+    console.log('construct 호출');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount 호출');
+    // this.setState({ updated: true });
+    this.forceUpdate();
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate 호출');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount 호출');
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log('getSnapshotBeforeUpdate 호출');
+    return {};
+  }
+
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate 호출');
+    return false;
+  }
+
+  render() {
+    console.log('reder 호출');
+    return null;
+  }
+}
+
+export default LifecycleExample;
--- do-it-example/src/App.jsx
+++ do-it-example/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import Example from './03/Counter';
+import Example from './03/LifecycleExample';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasDestroyed: false };
+  }
+
+  componentDidMount() {
+    this.setState({ hasDestroyed: true });
+  }
+
   render() {
     return (
       <div>
-        <Example />
+        <div>{this.state.hasDestroyed ? null : <Example />}</div>
       </div>
     );
   }
