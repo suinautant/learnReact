53cfc3652486ed0f902ecdad03fe76bbd8249176
suinautant <suinautant@gmail.com>
2021-11-17 12:43:02 +0900

    admin state

--- do-it-example/src/App.jsx
+++ do-it-example/src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import ChildProperty from './03/ChildProperty';
+import StateExample from './03/StateExample';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <ChildProperty>
-          <div>
-            <span>자식 노드</span>
-          </div>
-        </ChildProperty>
+        <StateExample />
       </div>
     );
   }
