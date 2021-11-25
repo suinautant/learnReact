70aed55af3836664f84466a078d595990b7b51cc
suinautant <suinautant@gmail.com>
2021-11-25 12:11:54 +0900

    리덕스 기초

---
+++ do-it-example/src/stories/ReduxStory.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import ReduxApp01 from '../07/ReduxApp01';
+
+storiesOf('Redux', module).addWithJSX('default', () => <ReduxApp01>리덕스 예제</ReduxApp01>);
