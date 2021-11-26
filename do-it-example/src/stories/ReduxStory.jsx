3aeffeaa0107acd8c21fa5698102f18f6399e435
suinautant <suinautant@gmail.com>
2021-11-26 14:17:19 +0900

    액션과 리듀서 관계

--- do-it-example/src/stories/ReduxStory.jsx
+++ do-it-example/src/stories/ReduxStory.jsx
@@ -2,5 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import ReduxApp01 from '../07/ReduxApp01';
+import ReduxApp02 from '../07/ReduxApp02';
 
-storiesOf('Redux', module).addWithJSX('default', () => <ReduxApp01>리덕스 예제</ReduxApp01>);
+storiesOf('Redux', module)
+  .addWithJSX('app01', () => <ReduxApp01>리덕스 예제</ReduxApp01>)
+  .addWithJSX('app02', () => <ReduxApp02>리덕스 예제</ReduxApp02>);
