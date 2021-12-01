31bc3d2d90a9ebb1df6fa11a9960907e59ab766e
suinautant <suinautant@gmail.com>
2021-12-01 18:44:04 +0900

    08-3/앱 전체 화면

--- do-it-example/src/index.js
+++ do-it-example/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './doit-ui/app.css';
+import CoinApp from './08/CoinApp';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CoinApp />, document.getElementById('root'));
+
+serviceWorker.unregister();
