2a441cd6aa805100529b554c3f90f9122cf4aa09
suinautant <suinautant@gmail.com>
2021-12-02 18:11:11 +0900

    09-3 리덕스 적용

--- do-it-example/src/08/components/main/TransactionList.jsx
+++ do-it-example/src/08/components/main/TransactionList.jsx
@@ -1,35 +1,18 @@
 import React, { PureComponent } from 'react';
-
+import Api from '../../Api';
 import Heading from './../../../doit-ui/Heading';
 import Card from './../../../doit-ui/Card';
+
 import TransactionSearchFilter from './TransactionSearchFilter';
 import TransactionTable from './TransactionTable';
 
-// import axios from 'axios';
-import Api from '../../Api';
-
 class TransactionList extends PureComponent {
-  state = {
-    transactions: [
-      // {
-      //   id: 'btx_01',
-      //   name: '비트코인(BTX)',
-      //   totalPrice: '123,123,000,000원',
-      //   currentPrice: '4,200,000원',
-      //   datetime: '2019/01/20 08:23:22',
-      // },
-    ],
-  };
-
   componentDidMount() {
-    Api
-      // .get('http://localhost:4000/transactions')
-      .get('http://localhost:4000/transactions', { params: { code: 'BTX' } })
-      .then((response) => this.setState({ transactions: response.data }));
+    Api.get('/transactions').then(({ data }) => this.props.setTransactionList(data));
   }
 
   render() {
-    const { transactions } = this.state;
+    const { transactions } = this.props;
     return (
       <div>
         <Heading level={3}>거래 현황</Heading>
@@ -43,5 +26,10 @@ class TransactionList extends PureComponent {
     );
   }
 }
+
+TransactionList.defaultProps = {
+  transactions: [],
+  setTransactionList: () => {},
+};
 
 export default TransactionList;
