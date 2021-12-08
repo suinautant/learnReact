8a00b28e2bd03dda76d3d342a5840011d08a52bf
suinautant <suinautant@gmail.com>
2021-12-08 19:41:47 +0900

    10-1 / 다중 미들웨어

--- do-it-example/src/07/configureStore.js
+++ do-it-example/src/07/configureStore.js
@@ -1,15 +1,6 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers';
 
-const customMiddleware = (store) => (nextRunner) => (action) => {
-  console.log('액션 객체', action);
-  console.log('리듀서 실행 전', store.getState());
-  const result = nextRunner(action);
-  console.log('리듀서 실행 후', store.getState());
-  return result;
-};
-
 export default (initStates) =>
-  // createStore(combineReducers(reducers), initStates, composeWithDevTools());
-  createStore(combineReducers(reducers), initStates, applyMiddleware(customMiddleware));
+  createStore(combineReducers(reducers), initStates, composeWithDevTools());
--- do-it-example/src/08/store/configureStore.js
+++ do-it-example/src/08/store/configureStore.js
@@ -1,6 +1,46 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { SET_TRANSACTION_LIST } from '../actions/transactionActions';
 import reducers from '../reducers';
 
+const customMiddleware = (store) => (nextRunner) => (action) => {
+  console.log('미들웨어에 전달된 액션 객체', action);
+  console.log('리듀서 실행이전', store.getState());
+  const result = nextRunner(action);
+  console.log('리듀서 실행이후', store.getState());
+  return result;
+};
+
+const customMidleware1 = () => (nextRunner) => (action) => {
+  if (action.type === SET_TRANSACTION_LIST) {
+    return nextRunner({
+      ...action,
+      payload: [
+        {
+          id: 0,
+          code: 'DOIT',
+          name: '두잇코인(DOIT)',
+          totalPrice: 100000000,
+          currentPrice: 25000,
+          datetime: '현재시간',
+        },
+      ],
+    });
+  }
+  return nextRunner(action);
+};
+
+const customMiddleware2 = (store) => (nextRunner) => (action) => {
+  console.log('미들웨어2 전달된 액션 객체', action);
+  console.log('미들웨어2 실행이전', store.getState());
+  const result = nextRunner(action);
+  console.log('미들웨어2 실행이후', store.getState());
+  return result;
+};
+
 export default (initState) =>
-  createStore(combineReducers(reducers), initState, composeWithDevTools());
+  createStore(
+    combineReducers(reducers),
+    initState,
+    composeWithDevTools(applyMiddleware(customMiddleware, customMiddleware2)),
+  );
